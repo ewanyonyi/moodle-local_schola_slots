18d8d3cbdc8bf0372d41b7d42511c2590559c0a9
ewanyonyi <wanyonyi.d.emanuel@gmail.com>
2026-08-22 18:34:27 +0300

    chore(release): bump version to 1.0.9 (2026082208)

--- version.php
+++ version.php
@@ -26,7 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_schola_slots';
-$plugin->version   = 2026082207;
+$plugin->version   = 2026082208;
 $plugin->requires  = 2024042200;
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = '1.0.8';
+$plugin->release   = '1.0.9';
